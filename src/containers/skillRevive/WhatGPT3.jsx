80a3946fb891be791eedf17f7f346858616bbe15
sakshi3326 <saqkshiiitian123@gmail.com>
2024-02-25 20:59:19 +0530

    landing page part3

--- src/containers/skillRevive/WhatGPT3.jsx
+++ src/containers/skillRevive/WhatGPT3.jsx
@@ -5,16 +5,17 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is SkillRevive" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="What is SkillRevive" text="It's a platform for freelancers to connect with clients seeking expertise, where both seasoned professionals and newcomers alike can gain skills and income opportunities Even clients can grasp the execution of their projects and the methodologies employed by freelancers, facilitating a comprehensive understanding of the process via Video chat." />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
-      <p>Explore the Library</p>
+      {/* <p>Explore the Library</p> */}
     </div>
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Workfl-ights" text="With Workfli-ghts, we provide a distinctive offering where not only do you receive your project on time, but you also have the opportunity to learn and comprehend the journey leading to its completion. Freelancers guide you through this process via video chat." />
+      <Feature title="R.T.D" text="R.T.D or Rapid Talent Dispatch is a commitment to providing a freelancer within 24 hours, or else our proficient team will take charge of the task." />
+      <Feature title="Facilitator" text="A dedicated individual will support and guide you throughout the project, ensuring clear communication and understanding of the process, with payment securely held in escrow." />
+      <Feature title="Empowerment Zone" text="Freelancers who are just starting out or struggling to secure projects can register on this platform, providing them with an opportunity to learn, grow, and earn income concurrently." />    
     </div>
   </div>
 );
