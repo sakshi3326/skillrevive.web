c66e480317ca9ad780ef65686a365e47339e961e
sakshi3326 <saqkshiiitian123@gmail.com>
2024-02-27 02:00:13 +0530

    landing page update

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -24,8 +24,8 @@ const featuresData = [
 const Features = () => (
   <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
-      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
-      <p>Request Early Access to Get Started</p>
+      <h1 className="gradient__text">Remain updated on contemporary trends and utilize your capabilities to capitalize on these burgeoning fields, thereby yielding revenue.</h1>
+      <p>Start Your Journey Here</p>
     </div>
     <div className="gpt3__features-container">
       {featuresData.map((item, index) => (
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import possibilityImage from '../../assets/possibility.png';
+import possibilityImage from '../../assets/noperson.jpeg';
 import './possibility.css';
 
 const Possibility = () => (
@@ -8,10 +8,10 @@ const Possibility = () => (
       <img src={possibilityImage} alt="possibility" />
     </div>
     <div className="gpt3__possibility-content">
-      <h4>Request Early Access to Get Started</h4>
+      {/* <h4>Request Early Access to Get Started</h4> */}
       <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-      <h4>Request Early Access to Get Started</h4>
+      <p>Equip yourself to work alongside us, where you'll encounter numerous possibilities for project completion, skill advancement, and revenue generation, all while securing regular work opportunities.</p>
+      <h4>Start Your Journey Here</h4>
     </div>
   </div>
 );
